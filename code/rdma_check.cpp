2461a97f1d641283828336acd37ade72a7396adc
nguyuyen <nguyuyennguyen@gmail.com>
2025-11-08 22:13:40 +0700

    add note

--- code/rdma_check.cpp
+++ code/rdma_check.cpp
@@ -16,21 +16,30 @@ int main(int argc, char** argv) {
   MPI_Win_fence(0, w);
   MPI_Win_lock_all(0, w);
 
+  if (myrank == 0) {
+    std::cout << "Start" << std::endl;
+  }
+
   MPI_Barrier(MPI_COMM_WORLD);
 
   int temp = myrank;
 
   int result = 0;
+  int result2 = 0;
 
   for (int i = 0; i < 100; ++i) {
     MPI_Get_accumulate(NULL, 0, MPI_INT, &result, 1, MPI_INT, 0, 0, 1, MPI_INT, MPI_NO_OP, w);
     MPI_Win_flush(0, w);
-    MPI_Accumulate(&temp, 1, MPI_INT, 0, 0, 1, MPI_INT, MPI_REPLACE, w);
+    MPI_Compare_and_swap(&temp, &result, &result2, MPI_INT, 0, 0, w);
     MPI_Win_flush(0, w);
   }
 
   MPI_Win_unlock_all(w);
   MPI_Win_free(&w);
+
+  if (myrank == 0) {
+    std::cout << "OK" << std::endl;
+  }
 
   MPI_Finalize();
   return 0;
